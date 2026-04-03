1c60faeeb06ae97b6c7bc4e7f1f67ce6a7cf6a9c
BlueSpark MZ <bluesparkmz@users.noreply.github.com>
2026-04-03 19:12:02 +0200

    d

--- src/types/screen.ts
+++ src/types/screen.ts
@@ -1,1 +1,1 @@
-export type Screen = "pdv" | "overview" | "products" | "stock" | "reports" | "settings" | "tables" | "sales" | "tabs" | "categories" | "fastfood";
+export type Screen = "pdv" | "overview" | "products" | "stock" | "reports" | "settings" | "tables" | "sales" | "tabs" | "categories" | "fastfood" | "finance";
