a4f90fe20a2b5ed9642e791ab5fe0c2089380833
BlueSpark MZ <bluesparkmz@users.noreply.github.com>
2026-04-05 10:41:09 +0200

    ola

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && componentTagger(),
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: null,
       includeAssets: ["pwa-icon.svg", "robots.txt", "placeholder.svg"],
       manifest: {
         id: "/",
