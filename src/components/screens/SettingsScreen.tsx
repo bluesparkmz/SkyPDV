23bc8a97dec098fbaac8083a6580d52346d9fb7d
BlueSpark MZ <bluesparkmz1@gmail.com>
2026-04-22 11:49:05 +0200

    dd

--- src/components/screens/SettingsScreen.tsx
+++ src/components/screens/SettingsScreen.tsx
@@ -54,6 +54,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
 
 const useStyles = makeStyles({
   root: {
